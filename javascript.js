166ad524da5d8fa65a436a267c29d0df7180d1be
Stillwell-C <danceswithhodge@gmail.com>
2022-03-22 17:37:39 +0900

    Changed operator variable  to use strings

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-let operator
+let operator = 'add'
 let total = 0
 
 
 function operate(num1, num2){
-    if (operator = add){
+    if (operator == 'add'){
         return total = num1 + num2;
-    } else if (operator = subtract) {
+    } else if (operator == 'subtract') {
         return total = num1 - num2;
-    } else if (operator = multiply) {
+    } else if (operator == 'multiply') {
         return total = num1 * num2;
-    } else if (operator = divide) {
+    } else if (operator == 'divide') {
         return total = num1 / num2;
     }
 }
