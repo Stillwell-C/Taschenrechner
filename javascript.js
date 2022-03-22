c7ab647945fd5ce850b3fcb61a440e16d1e2e810
Stillwell-C <danceswithhodge@gmail.com>
2022-03-22 17:30:03 +0900

    Add operate() to handle math computations

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let operator
+
+
+function operate(num1, num2){
+    if (operator = add){
+        return num1 + num2;
+    } else if (operator = subtract) {
+        return num1 - num2;
+    } else if (operator = multiply) {
+        return num1 * num2;
+    } else if (operator = divide) {
+        return num1 / num2;
+    }
+}
