1a14515f6d6e23be0eddbc9de4770d431bf65528
Stillwell-C <danceswithhodge@gmail.com>
2022-03-23 10:42:28 +0900

    Add screenUpdate() and calculate().
    screenUpdate() updates the text in the screenDisplay div
    to show the current displayValue.
    calculate() calls operate() and screenUpdate() to run calculation and
    show result on screen.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 let operator
 let currentValue = 0
-let displayValue = 0
+let displayValue = 0 //Make array?
 let num1
 let num2
 const container = document.querySelector('#main-body')
 
 function operate(num1, num2){
     if (operator == 'add'){
-        return displayValue = num1 + num2
+        return displayValue = num1 + num2;
     } else if (operator == 'subtract') {
         return displayValue = num1 - num2;
     } else if (operator == 'multiply') {
@@ -17,36 +17,89 @@ function operate(num1, num2){
     }
 }
 
+function calculate() {
+    operate(num1, displayValue);
+    screenUpdate()
+}
+
 // Assign value when number button is clicked
-const sevenBtn = document.querySelector('#btn7');
-sevenBtn.addEventListener('click', () => displayValue = 7) 
+const sevenBtn = document.querySelector('#b7');
+sevenBtn.addEventListener('click', () => {
+    displayValue = getID(sevenBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const eightBtn = document.querySelector('#btn8');
-eightBtn.addEventListener('click', () => displayValue = 8)
+const eightBtn = document.querySelector('#b8');
+eightBtn.addEventListener('click', () => {
+    displayValue = getID(eightBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const nineBtn = document.querySelector('#btn9');
-nineBtn.addEventListener('click', () => displayValue = 9)
+const nineBtn = document.querySelector('#b9');
+nineBtn.addEventListener('click', () => {
+    displayValue = getID(nineBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const fourBtn = document.querySelector('#btn4');
-fourBtn.addEventListener('click', () => displayValue = 4)
+const fourBtn = document.querySelector('#b4');
+fourBtn.addEventListener('click', () => {
+    displayValue = getID(fourBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const fiveBtn = document.querySelector('#btn5');
-fiveBtn.addEventListener('click', () => displayValue = 5)
+const fiveBtn = document.querySelector('#b5');
+fiveBtn.addEventListener('click', () => {
+    displayValue = getID(fiveBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const sixBtn = document.querySelector('#btn6');
-sixBtn.addEventListener('click', () => displayValue = 6)
+const sixBtn = document.querySelector('#b6');
+sixBtn.addEventListener('click', () => {
+    displayValue = getID(sixBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const oneBtn = document.querySelector('#btn1');
-oneBtn.addEventListener('click', () => displayValue = 1)
+const oneBtn = document.querySelector('#b1');
+oneBtn.addEventListener('click', () => {
+    displayValue = getID(oneBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const twoBtn = document.querySelector('#btn2');
-twoBtn.addEventListener('click', () => displayValue = 2)
+const twoBtn = document.querySelector('#b2');
+twoBtn.addEventListener('click', () => {
+    displayValue = getID(twoBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const threeBtn = document.querySelector('#btn3');
-threeBtn.addEventListener('click', () => displayValue = 3)
+const threeBtn = document.querySelector('#b3');
+threeBtn.addEventListener('click', () => {
+    displayValue = getID(threeBtn);
+    screenUpdate();
+    return displayValue;
+});
 
-const zeroBtn = document.querySelector('#btn0');
-zeroBtn.addEventListener('click', () => displayValue = 0)
+const zeroBtn = document.querySelector('#b0');
+zeroBtn.addEventListener('click', () => {
+    displayValue = getID(zeroBtn);
+    screenUpdate();
+    return displayValue;
+});
+
+//Function to get value from button id
+
+function getID(btn) {
+    let idValue = btn.id;
+    idValue = idValue.substr(-1, 1);
+    return parseInt(idValue);
+}
 
 //Assign operation to operator buttons
 //Removed num2 option for now due to error
@@ -79,7 +132,8 @@ addBtn.addEventListener('click', () => {
 //Make equal button call operate function
 const eqlBtn = document.querySelector('#eqlBtn');
 eqlBtn.addEventListener('click', () => {
-    operate(num1, displayValue);
+    // operate(num1, displayValue);
+    calculate();
     });
 
 //Clear button makes num 1, num 2, & display value zero
@@ -88,4 +142,19 @@ clrBtn.addEventListener('click', () => {
     num1 = 'undefined'
     num2 = 'undefined'
     displayValue = 0
+    screenUpdate();
 })
+
+//Make claculations appear on screen
+const screenContainer = document.querySelector('#screen')
+
+const screenDisplay = document.createElement('div');
+screenDisplay.classList.add('screenDisplay');
+screenDisplay.textContent = displayValue;
+screenContainer.appendChild(screenDisplay);
+
+//Function to update screen content
+function screenUpdate() {
+    screenDisplay.textContent = displayValue;
+    screenContainer.appendChild(screenDisplay);
+}
